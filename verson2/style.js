622df5e8378ad93e94ea4e9b112c44cd515e93e6
Hao Ping Yu <pumpkinjack@livemail.tw>
2014-07-01 15:23:41 +0800

    ver2 finish

--- verson2/style.js
+++ verson2/style.js
@@ -16,11 +16,29 @@ $(".next").click(function(){
     /*Active the processBar*/
     $("#processBar li").eq($('#mutiStep fieldset').index(next_fs)).addClass('active');
     
-    //Load the next fieldset & hide the current.
-    current_fs.fadeOut(300);
-    next_fs.fadeIn(300);
+    //show the next fieldset
+    next_fs.show(); 
     
-    animating = false; // renew the multi-click flag;
+    current_fs.animate({opacity: 0}, {
+        step: function(now, mx) {
+			//1. scale current_fs down to 80%
+			scale = 0.8+ now* 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'transform': 'scale('+scale+')'});
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+        duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false; // renew the multi-click flag;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+    });
+    
 });
 
 $(".pre").click(function(){
@@ -35,9 +53,26 @@ $(".pre").click(function(){
     /*cancel the processBar*/
     $("#processBar li").eq($('#mutiStep fieldset').index(current_fs)).removeClass('active');
     
-    //Load the next fieldset & hide the current.
-    current_fs.fadeOut(300);
-    prev_fs.fadeIn(300);
+    //show the next fieldset
+    prev_fs.show(); 
     
-    animating = false; // renew the multi-click flag;
+    current_fs.animate({opacity: 0}, {
+        step: function(now, mx) {
+			//1. scale prev_fs down from 80%
+			scale = 1-(0.2*now);
+			//2. bring current_fs to the right(50%)
+			left = ((1-now) * 50 )+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({'left': left });
+			prev_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+        duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false; // renew the multi-click flag;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+    });
 });
